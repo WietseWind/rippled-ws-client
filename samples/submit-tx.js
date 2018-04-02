8bbada3174e05a97185579f1fb2def6bda34448f
Wietse Wind <w.wind@ipublications.net>
2018-04-02 14:12:30 +0200

    RM Sample

--- samples/submit-tx.js
+++
@@ -1,8 +0,0 @@
-/**
- * Moved! No need to sign, submit and await manually!
- * 
- * NPM:
- *    https://www.npmjs.com/package/rippled-ws-client-sign
- * Samples:
- *    https://github.com/WietseWind/rippled-ws-client-sign/blob/master/samples
- */
